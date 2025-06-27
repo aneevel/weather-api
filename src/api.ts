08cc2c81cdf587c6471c8c7c1ba00539a9472084
aneevel <aneevel15@gmail.com>
2025-06-26 22:41:14 -0500

    create basic server

---
+++ src/api.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Weather API');
+});
+
+app.listen(port, () => {
+  console.log(`Weather API running on port ${port}`);
+})
